842e1ca065d537baff33d89afb4fda08d10480c9
CodzSword <124258380+codzsword@users.noreply.github.com>
2023-08-05 00:12:11 +0545

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Sidebar toggle
+
+// theme toggle
+
+document.querySelector(".theme-toggle").addEventListener("click", () => {
+  toggleLocalStorage();
+  toggleRootClass();
+});
+
+function toggleRootClass() {
+  const current = document.documentElement.getAttribute("data-bs-theme");
+  const inverted = current == "dark" ? "light" : "dark";
+  document.documentElement.setAttribute("data-bs-theme", inverted);
+}
+
+function toggleLocalStorage() {
+  if (isLight()) {
+    localStorage.removeItem("light");
+  } else {
+    localStorage.setItem("light", "set");
+  }
+}
+
+function isLight() {
+  return localStorage.getItem("light");
+}
+
+if (isLight()) {
+  toggleRootClass();
+}
