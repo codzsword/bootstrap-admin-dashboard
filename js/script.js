4148bd1dfcfddbb5bef31efb6e46e9a2a24a392b
CodzSword <124258380+codzsword@users.noreply.github.com>
2023-12-10 13:58:09 +0545

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,31 @@
-// Sidebar toggle
-
-// theme toggle
-
-document.querySelector(".theme-toggle").addEventListener("click", () => {
-  toggleLocalStorage();
-  toggleRootClass();
+const sidebarToggle = document.querySelector("#sidebar-toggle");
+sidebarToggle.addEventListener("click",function(){
+    document.querySelector("#sidebar").classList.toggle("collapsed");
 });
 
-function toggleRootClass() {
-  const current = document.documentElement.getAttribute("data-bs-theme");
-  const inverted = current == "dark" ? "light" : "dark";
-  document.documentElement.setAttribute("data-bs-theme", inverted);
+document.querySelector(".theme-toggle").addEventListener("click",() => {
+    toggleLocalStorage();
+    toggleRootClass();
+});
+
+function toggleRootClass(){
+    const current = document.documentElement.getAttribute('data-bs-theme');
+    const inverted = current == 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-bs-theme',inverted);
 }
 
-function toggleLocalStorage() {
-  if (isLight()) {
-    localStorage.removeItem("light");
-  } else {
-    localStorage.setItem("light", "set");
-  }
+function toggleLocalStorage(){
+    if(isLight()){
+        localStorage.removeItem("light");
+    }else{
+        localStorage.setItem("light","set");
+    }
 }
 
-function isLight() {
-  return localStorage.getItem("light");
+function isLight(){
+    return localStorage.getItem("light");
 }
 
-if (isLight()) {
-  toggleRootClass();
+if(isLight()){
+    toggleRootClass();
 }
